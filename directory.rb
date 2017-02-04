b8be6264b4ab0132330362227b76d235702d9f3a
Ryan <Ryanchu.ws@gmail.com>
2017-02-04 22:24:26 +0000

    refactored interactive

--- directory.rb
+++ directory.rb
@@ -1,40 +1,49 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+@students = []
 
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "Invalid selection. Try again"
-      end
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Invalid selection. Try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
 
   name = gets.chomp
 #while name is NOT empty (meaning user has inputted a value), repeat
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #continue getting names ->
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -42,14 +51,14 @@ def print_header
   puts "-" * 10
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
